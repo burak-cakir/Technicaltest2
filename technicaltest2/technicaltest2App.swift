b486c1addb051147fd01fc22e7d5618ce8060103
burak-cakir <burakcakir@hotmail.com.tr>
2022-03-13 12:32:05 +0300

    Initial Commit

---
+++ technicaltest2/technicaltest2App.swift
@@ -0,0 +1,17 @@
+//
+//  technicaltest2App.swift
+//  technicaltest2
+//
+//  Created by burak cakir on 13.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct technicaltest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
